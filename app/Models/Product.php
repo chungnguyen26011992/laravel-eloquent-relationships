fce10a4190803660a2f1ee676eb9ae8bf7053c22
Chung Nguyen <chung.nguyen1@tda.company>
2021-06-28 18:07:09 +0700

    update modal Category, Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,6 +19,6 @@ class Product extends Model
     ];
 
     public function categories() {
-        return $this->belongsToMany(Category::class, 'category_product', 'category_id', 'product_id');
+        return $this->belongsToMany(Category::class, 'category_product');
     }
 }
